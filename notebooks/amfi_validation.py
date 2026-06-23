5520024c00bbdbd790476c7840803ad69e20b6b2
kumar-byte <mkumar776644@gmail.com>
2026-06-24 00:04:32 +0530

    Day 2: Cleaned data + Mysql DB loaded

--- notebooks/amfi_validation.py
+++ notebooks/amfi_validation.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+#amfi_validation
 fund_master = pd.read_csv("data/raw/01_fund_master.csv")
 nav_history = pd.read_csv("data/raw/02_nav_history.csv")
 
--- notebooks/load_to_sqlite.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sqlalchemy import create_engine
-
-engine = create_engine("sqlite:///bluestock_mf")
-
-print("Database created successfully!")
